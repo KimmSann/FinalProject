e0bde31ff49ffe6447479ffe3eee5d2024f4d30a
G201 <G201@DESKTOP-2R01S2Q>
2025-07-14 17:48:11 +0900

    로그인 테스트

--- src/main/java/com/example/demo/kakao/Service/KakaoServiceImpl.java
+++ src/main/java/com/example/demo/kakao/Service/KakaoServiceImpl.java
@@ -1,11 +1,10 @@
-package com.example.demo.kakao.Service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class KakaoServiceImpl implements KakaoService {
-    @Override
-    public String getAccessTokenFromKakao(String code, String redirectUri) {
-        // 실제 구현
-    }
-}
+//package com.example.demo.kakao.Service;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class KakaoServiceImpl implements KakaoService {
+//    public String getAccessTokenFromKakao(String code, String redirectUri) {
+//        // 실제 구현
+//    }
+//}
--- src/test/java/com/example/demo/service/UserTest.java
+++ src/test/java/com/example/demo/service/UserTest.java
@@ -1,33 +1,39 @@
 package com.example.demo.service;
 
-import java.time.LocalDateTime;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.user.entity.User;
 import com.example.demo.user.repository.UserRepository;
-import com.example.demo.user.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 public class UserTest {
-	
-	@Autowired
-	UserRepository repository;
-	
-	@Autowired
-	UserService service;
-	
-	void 유저_하나_저장_테스트() {  
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void saveTestUserWithEncodedPassword() {
+        String rawPassword = "1234";
+        String encodedPassword = passwordEncoder.encode(rawPassword); 
+
         User user = User.builder()
-                .name("testuser")
-                .email("test@example.com")
-                .password("1234")
-                .nickname("테스트유저")
-                .profileimg("http://example.com/profile.jpg")
+                .name("홍길동")
+                .email("hong@example.com")
+                .password(encodedPassword)
+                .nickname("길동이")
+                .profileimg("default.png")
                 .role("USER")
-                .createdate(LocalDateTime.now())
                 .build();
- }
+
+        userRepository.save(user);
+
+
+    }
 }
+

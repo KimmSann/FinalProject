179109acfd3ecc21514aa3741d1876ef659bb7dd
imjiyeon <yoyt12@naver.com>
2025-07-18 12:01:06 +0900

    ㅇㅇㅇ

--- src/main/java/com/example/demo/config/UserDetailService.java
+++ src/main/java/com/example/demo/config/UserDetailService.java
@@ -1,4 +1,4 @@
-package com.example.demo.user.repository;
+package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
